f40f4e0dd99dcbb2a9152f3533a3d1559c2e159d
marcelosoliveira <msbobsk8@gmail.com>
2021-04-30 19:14:02 -0300

    Implementando o Swagger

--- src/main/java/br/com/centralerrors/config/SwaggerConfig.java
+++ src/main/java/br/com/centralerrors/config/SwaggerConfig.java
@@ -3,12 +3,16 @@ package br.com.centralerrors.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -23,6 +27,13 @@ public class SwaggerConfig {
                     .apis(RequestHandlerSelectors.basePackage("br.com.centralerrors"))
                     .paths(regex("/v1.*"))
                     .build()
+                .globalOperationParameters(Collections.singletonList(new ParameterBuilder()
+                    .name("Authorization")
+                    .description("Bearer Token")
+                    .modelRef(new ModelRef("string"))
+                    .parameterType("header")
+                    .required(true)
+                    .build()))
                 .apiInfo(metaData());
     }
 
