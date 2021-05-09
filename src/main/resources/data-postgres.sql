94b9dcab294de6769cc934e6fd50a50b6e55cc6a
marcelosoliveira <msbobsk8@gmail.com>
2021-05-09 14:25:50 -0300

    Atualizando a api para apresentação

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,8 +1,6 @@
 INSERT INTO users(
 	id, email, is_admin, name, password, user_name)
-	VALUES (1, 'msbobsk8@homail.com', true, 'Marcelo dos Santos',
-	        '$2a$10$QCZ6ITTPxg2214sh9JKTwuxr37EGNbMRPL3Kc.CoHyA99EBQXMAU2', 'bob'),
-	       (2, 'sagacello@hotmail.com', true, 'Marcelo Ivan Santos',
-	        '$2a$10$QCZ6ITTPxg2214sh9JKTwuxr37EGNbMRPL3Kc.CoHyA99EBQXMAU2', 'saga'),
-	       (3, 'ricardoheckalves@icloud.com', true, 'Ricardo Alves',
-	        '$2a$10$QCZ6ITTPxg2214sh9JKTwuxr37EGNbMRPL3Kc.CoHyA99EBQXMAU2', 'onfire');
+	VALUES (1, 'contaazul@gmail.com', true, 'Conta Azul Admin',
+	        '$2a$10$QCZ6ITTPxg2214sh9JKTwuxr37EGNbMRPL3Kc.CoHyA99EBQXMAU2', 'contaazul'),
+	       (2, 'contaazul@gmail.com', true, 'Conta Azul User',
+	        '$2a$10$QCZ6ITTPxg2214sh9JKTwuxr37EGNbMRPL3Kc.CoHyA99EBQXMAU2', 'azul');
