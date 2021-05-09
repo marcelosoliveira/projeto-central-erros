322216a72adebfbe676c7ddc7fb91dd034af6c9a
marcelosoliveira <msbobsk8@gmail.com>
2021-05-09 14:43:58 -0300

    Atualizando a api para apresentação

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(
 	id, email, is_admin, name, password, user_name)
-	VALUES (1, 'contaazul@gmail.com', true, 'Conta Azul Admin',
+	VALUES ('contaazul@gmail.com', true, 'Conta Azul Admin',
 	        '$2a$10$QCZ6ITTPxg2214sh9JKTwuxr37EGNbMRPL3Kc.CoHyA99EBQXMAU2', 'contaazul'),
-	       (2, 'azul@gmail.com', false, 'Azul User',
+	       ('azul@gmail.com', false, 'Azul User',
 	        '$2a$10$QCZ6ITTPxg2214sh9JKTwuxr37EGNbMRPL3Kc.CoHyA99EBQXMAU2', 'azul');
