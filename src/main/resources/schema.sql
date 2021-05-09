2885917d61f6d313bb1bc6534f71fdfa1fd80472
marcelosoliveira <msbobsk8@gmail.com>
2021-05-09 14:32:15 -0300

    Atualizando a api para apresentação

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE users
-(
-    id INTEGER NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    is_admin boolean NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    user_name VARCHAR(255) NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id),
-    CONSTRAINT uk_k8d0f2n7n88w1a16yhua64onx UNIQUE (user_name)
-);
+--CREATE TABLE users
+--(
+--    id INTEGER NOT NULL,
+--    email VARCHAR(255) NOT NULL,
+--    is_admin boolean NOT NULL,
+--    name VARCHAR(255) NOT NULL,
+--    password VARCHAR(255) NOT NULL,
+--    user_name VARCHAR(255) NOT NULL,
+--    CONSTRAINT users_pkey PRIMARY KEY (id),
+--    CONSTRAINT uk_k8d0f2n7n88w1a16yhua64onx UNIQUE (user_name)
+--);
 
 --CREATE TABLE public.events
 --(
